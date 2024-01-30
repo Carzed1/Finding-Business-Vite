ce68f989d8285f6d3487e22fd8133d6da939806b
Chaitanya Gupta <guptadivya958@gmail.com>
2024-01-30 16:15:54 +0530

    commit successful

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {
   Navbar,
   Home,
   About,
-  Teacher,
+  
   Contact,
   Courses,
   Community,
@@ -17,11 +17,12 @@ function App() {
       <Home />
       <About />
       <Courses />
-      <Teacher />
+   
       <Community />
       <Faq/>
       <Contact />
       <Footer />
+     
       
     </div>
   );
